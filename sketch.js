7483d6fb53cbc021ce36cd7146f868c159cd0a70
PurvaaBarot <81853659+PurvaaBarot@users.noreply.github.com>
2021-04-12 22:50:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,98 @@
+var starImg,bgImg;
+var star, starBody;
+//create variable for fairy sprite and fairyImg
+var fairy,fairyImg,fairyBody;
+
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	bgImg = loadImage("images/starNight.png");
+	//load animation for fairy here
+	fairyImg=loadAnimation("images/fairyImage1.png", "images/fairyImage2.png");
+	//fairyVoice=loadSound("sound/JoyMusic.mp3");
+
+}
+
+function setup() {
+	createCanvas(800, 750);
+    
+	//write code to play fairyVoice sound
+   // playSound("JoyMusic.mp3");
+	//create fairy sprite and add animation for fairy
+   fairy=createSprite(200,500);
+   fairy.addAnimation("fairy",fairyImg);
+   fairy.scale=0.3;
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+  background(bgImg);
+
+  star.x= starBody.position.x 
+  star.y= starBody.position.y 
+
+  console.log(star.y);
+
+  //write code to stop star in the hand of fairy
+   
+  if(star.y > 470 && starBody.position.y > 470){
+	  Matter.Body.setStatic(starBody,true)
+  }
+
+
+  drawSprites();
+
+}
+
+function keyPressed() {
+
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(starBody,false); 
+	}
+
+	//write code to move fairy left and right
+
+	if(keyCode === LEFT_ARROW){
+		Matter.Body.setStatic(fairyBody,false);
+		fairy.velocityX=fairy.velocityX-5;
+		
+	}
+
+	if(keyCode === RIGHT_ARROW){
+		fairy.velocityX=fairy.velocityX+5;
+	}
+	else{
+		fairy.velocityX=0
+		}
+
+
+
+	Engine.update(engine);
+
+	fairyMode(CENTER);
+	 fairy(fairy.position.x,fairy.position.y,400,20); 
+	 
+	
+
+
+}
